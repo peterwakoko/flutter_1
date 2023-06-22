c645527b0eee620b1afb1015a8fdb8339ea8ad31
peterwakoko <peterwakoko@gmail.com>
2023-06-22 12:55:21 +0300

    organised dart code

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import "package:flutter/material.dart";
+import './app_screens/first_screen.dart';
 
-void main(){
-  runApp(
+void main() => runApp(new MyFlutterApp());
 
-    MaterialApp(
-      title: "My Flutter App",
-      home: Scaffold(
-        appBar: AppBar(title: Text("My first App Screen"),),
-        body: Material(
-          color: Colors.lightBlueAccent,
-          child: Center(
-            child: Text(
-              "Hello world",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-          ),
-        ),
-      )
-    )
-  );
+class MyFlutterApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        title: "My Flutter App",
+        home: Scaffold(
+          appBar: AppBar(title: Text("My first App Screen"),),
+          body: FirstScreen()
+        )
+    );
+  }
+
 }
